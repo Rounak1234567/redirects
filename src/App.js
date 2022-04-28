2236e87ade77b2a424e4cca2af457d55589f542f
Rounak <riz.moj.msd97@gmail.com>
2022-04-28 23:05:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/2" element={<Home2 />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+function Home() {
+  return (
+    <>
+        <h2>Welcome to the Home page!</h2>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+        <h2>Welcome to the About page!</h2>
+      <nav>
+        <Link to="/2">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function Home2() {
+  return (
+    <>
+        <h2>Welcome to the Home page 2!</h2>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+
+
+
+
+export default App;
